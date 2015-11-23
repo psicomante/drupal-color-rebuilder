f6621fefdb1edf48fcdcc49e2f761bc5f7891632
Roberto Pesando <roberto.pesando@gmail.com>
2015-11-23 18:37:24 +0100

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+Color Rebuilder
+===============
+
+TODO: write some documentation.
---
+++ color_rebuilder.api.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file color_rebuilder.api.php
+ * Hooks provided by the Color Rebuilder module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
